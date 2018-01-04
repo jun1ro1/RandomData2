3c4371424e40fa12e88b5c713bb16d9fae2852c3
jun1ro1 <jun1ro1@me.com>
2018-01-04 18:10:28 +0900

    Initial Commit

---
+++ RandomData2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomData2
+//
+//  Created by OKU Junichirou on 2018/01/04.
+//  Copyright © 2018年 OKU Junichirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
